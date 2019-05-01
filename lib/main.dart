f78da0977f76d8aa648e2a24ec4daeb75869f493
gvrmhd <givary.mhd@gmail.com>
2019-05-01 16:40:34 +0700

    Change App Name, Launcher Icon & Title

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'QRC Info',
       debugShowCheckedModeBanner: false,
       routes: routesList,
       initialRoute: '/',
